7d6e71223876f60929a1083fa5b9151526f63003
robri <robri@DESKTOP-84S6J8T>
2020-07-15 09:10:12 -0300

    Projeto Criado - Initial Commit

---
+++ sistemadeemprestimos/src/main/java/br/edu/infnet/sistemadeemprestimos/SistemadeemprestimosApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.sistemadeemprestimos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemadeemprestimosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemadeemprestimosApplication.class, args);
+	}
+
+}
---
+++ sistemadeemprestimos/src/test/java/br/edu/infnet/sistemadeemprestimos/SistemadeemprestimosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.sistemadeemprestimos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemadeemprestimosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
